6c6b73f4320a3c2e16c79a0293315b8afe846206
OOHehir <40183643+OOHehir@users.noreply.github.com>
2024-05-02 17:11:52 +0100

    Added sensor

--- src/main.c
+++ src/main.c
@@ -7,7 +7,11 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/input/input.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 #define SLEEP_TIME_MS   1000
 
@@ -16,10 +20,44 @@
 #error "Unsupported board: led0 devicetree alias is not defined"
 #endif
 
+// #define TOUCH1_NODE DT_ALIAS(touch1)
+// #if !DT_NODE_HAS_STATUS(TOUCH1_NODE, okay)
+// #error "Unsupported board: touch1 devicetree alias is not defined"
+// #endif
+
+#ifndef CONFIG_SOC_SERIES_ESP32S3
+#error "Unsupported SoC series"
+#endif
+
+#ifndef CONFIG_PCNT_ESP32
+#error "CONFIG_PCNT_ESP32 not set"
+#endif
+
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-static void input_cb(struct input_event *evt) {
+static const struct device *get_pcnt_device(void) {
+  const struct device *const dev = DEVICE_DT_GET_ANY(espressif_esp32_pcnt);
 
+  if (dev == NULL) {
+    /* No such node, or the node does not have status "okay". */
+    printk("\nError: no device found.\n");
+    return NULL;
+  }
+
+  if (!device_is_ready(dev)) {
+    printk("\nError: Device \"%s\" is not ready; "
+           "check the driver initialization logs for errors.\n",
+           dev->name);
+    return NULL;
+  }
+
+  printk("Found device \"%s\", getting sensor data\n", dev->name);
+
+  return dev;
+}
+
+static void input_cb(struct input_event *evt) {
+    printf("Input event: %d\n", evt->type);
 }
 
 INPUT_CALLBACK_DEFINE(NULL, input_cb);
@@ -42,9 +80,21 @@ int main(void) {
 
     gpio_pin_set_dt(&led, 0);
 
+    const struct device *dev = get_pcnt_device();
+
+    if (dev == NULL) {
+        printf("Failed to get PCNT device\n");
+        return 0;
+    }
+
     while (1) {
+        struct sensor_value rotation = {0};
+
+        sensor_sample_fetch(dev);
+        sensor_channel_get(dev, SENSOR_CHAN_ROTATION, &rotation);
+        printf("%d", rotation.val1);
+
         k_msleep(SLEEP_TIME_MS);
-        printf(".");
     }
     return 0;
 }
