e6a5ede1231b15c8011edda2e8cfe16c6db2bcda
OOHehir <40183643+OOHehir@users.noreply.github.com>
2024-05-10 16:29:03 +0100

    Add button

--- src/main.c
+++ src/main.c
@@ -5,10 +5,10 @@
  */
 
 #include <stdio.h>
-#include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/input/input.h>
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
@@ -20,10 +20,10 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #error "Unsupported board: led0 devicetree alias is not defined"
 #endif
 
-// #define TOUCH1_NODE DT_ALIAS(touch1)
-// #if !DT_NODE_HAS_STATUS(TOUCH1_NODE, okay)
-// #error "Unsupported board: touch1 devicetree alias is not defined"
-// #endif
+#define SW0_NODE DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
 
 #ifndef CONFIG_SOC_SERIES_ESP32S3
 #error "Unsupported SoC series"
@@ -33,82 +33,110 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #error "CONFIG_PCNT_ESP32 not set"
 #endif
 
+#ifdef CONFIG_SOC_SERIES_ESP32C3
+#error "Unsupported SoC series"
+#endif
+
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec button =
+    GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
+static struct gpio_callback button_cb_data;
 
 static const struct device *get_pcnt_device(void) {
   const struct device *const dev = DEVICE_DT_GET_ANY(espressif_esp32_pcnt);
 
   if (dev == NULL) {
-    /* No such node, or the node does not have status "okay". */
-    printk("\nError: no device found.\n");
+    printf("\nError: no device found.\n");
     return NULL;
   }
 
   if (!device_is_ready(dev)) {
-    printk("\nError: Device \"%s\" is not ready; "
+    printf("\nError: Device \"%s\" is not ready; "
            "check the driver initialization logs for errors.\n",
            dev->name);
     return NULL;
   }
 
-  printk("Found device \"%s\", getting sensor data\n", dev->name);
+  printf("Found device \"%s\", getting sensor data\n", dev->name);
 
   return dev;
 }
 
-static void input_cb(struct input_event *evt) {
-    printf("Input event: %d\n", evt->type);
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+                    uint32_t pins) {
+  printf("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
 }
 
-INPUT_CALLBACK_DEFINE(NULL, input_cb);
-
 int main(void) {
-    k_msleep(SLEEP_TIME_MS);
+  k_msleep(SLEEP_TIME_MS);
 
-    printf("Startup of board: %s\n", CONFIG_BOARD);
+  printf("Startup of board: %s\n", CONFIG_BOARD);
 
-    if (!gpio_is_ready_dt(&led)) {
-        printf("GPIO not ready to for LED at %s pin %d\n", led.port->name, led.pin);
-        return 0;
-    }
-
-    int ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        printf("Failed to configure LED at %s pin %d\n", led.port->name, led.pin);
-        return 0;
-    }
-
-    gpio_pin_set_dt(&led, 0);
-
-    const struct device *dev = get_pcnt_device();
-
-    if (dev == NULL) {
-        printf("Failed to get PCNT device\n");
-        return 0;
-    }
-
-    int old_result = 0;
-
-    while (1) {
-        struct sensor_value rotation = {0};
-
-        int rc = sensor_sample_fetch(dev);
-        if (rc != 0) {
-          printf("Failed to fetch sample (%d)\n", rc);
-          return 0;
-        }
-
-        rc = sensor_channel_get(dev, SENSOR_CHAN_ROTATION, &rotation);
-        if (rc != 0) {
-          printf("Failed to get data (%d)\n", rc);
-          return 0;
-        }
-
-        if (rotation.val1 != old_result) {
-            gpio_pin_toggle_dt(&led);
-            old_result = rotation.val1;
-            printf("Rotation: %d\n", rotation.val1);
-        }
-    }
+  if (!gpio_is_ready_dt(&led)) {
+    printf("GPIO not ready to for LED at %s pin %d\n", led.port->name, led.pin);
     return 0;
+  }
+
+  int ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+  if (ret < 0) {
+    printf("Failed to configure LED at %s pin %d\n", led.port->name, led.pin);
+    return 0;
+  }
+
+  gpio_pin_set_dt(&led, 0);
+
+  if (!gpio_is_ready_dt(&button)) {
+    printf("Error: button device %s is not ready\n", button.port->name);
+    return 0;
+  }
+
+  ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+  if (ret != 0) {
+    printf("Error %d: failed to configure %s pin %d\n", ret, button.port->name,
+           button.pin);
+    return 0;
+  }
+
+  ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+  if (ret != 0) {
+    printf("Error %d: failed to configure interrupt on %s pin %d\n", ret,
+           button.port->name, button.pin);
+    return 0;
+  }
+
+  gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+  gpio_add_callback(button.port, &button_cb_data);
+  printf("Set up button at %s pin %d\n", button.port->name, button.pin);
+
+  const struct device *dev = get_pcnt_device();
+
+  if (dev == NULL) {
+    printf("Failed to get PCNT device\n");
+    return 0;
+  }
+
+  int old_result = 0;
+
+  while (1) {
+    struct sensor_value rotation = {0};
+
+    int rc = sensor_sample_fetch(dev);
+    if (rc != 0) {
+      printf("Failed to fetch sample (%d)\n", rc);
+      return 0;
+    }
+
+    rc = sensor_channel_get(dev, SENSOR_CHAN_ROTATION, &rotation);
+    if (rc != 0) {
+      printf("Failed to get data (%d)\n", rc);
+      return 0;
+    }
+
+    if (rotation.val1 != old_result) {
+      gpio_pin_toggle_dt(&led);
+      old_result = rotation.val1;
+      printf("Rotation: %d\n", rotation.val1);
+    }
+  }
+  return 0;
 }
